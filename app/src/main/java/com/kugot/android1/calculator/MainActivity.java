bc0882fc1c6a05f77cd765f1f3f35b4434019e82
Admin <uscmagnus@gmail.com>
2021-03-03 21:49:33 +0200

    Lesson3 1. Напишите обработку каждой кнопки из макета калькулятора. 2. Создайте объект с данными и операциями калькулятора. Продумайте, каким образом будете хранить введённые пользователем данные. 3. * Создайте макет калькулятора для горизонтальной ориентации экрана и отображайте его в ландшафтной ориентации.

--- app/src/main/java/com/kugot/android1/calculator/MainActivity.java
+++ app/src/main/java/com/kugot/android1/calculator/MainActivity.java
@@ -1,14 +1,41 @@
 package com.kugot.android1.calculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private Button mButton00;
+    private Button mButton0;
+    private Button mButton1;
+    private Button mButton2;
+    private Button mButton3;
+    private Button mButton4;
+    private Button mButton5;
+    private Button mButton6;
+    private Button mButton7;
+    private Button mButton8;
+    private Button mButton9;
+    private Button mButtonLeftBracket;
+    private Button mButtonRightBracket;
+    private Button mButtonSqrt;
+    private Button mButtonDelimiter;
+    private Button mButtonMultiply;
+    private Button mButtonMinus;
+    private Button mButtonPlus;
+    private Button mButtonPlusMinus;
+    private Button mButtonSquare;
+    private Button mButtonP;
+    private Button mButtonResult;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void initialize(){
+
     }
 }
